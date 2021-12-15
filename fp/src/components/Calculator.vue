b36c64f46d2d6c8b290901656ab930e40e63c075
Николай Федоров <nvfedorov@cloudmts.ru>
2021-12-15 10:01:17 +0300

    finish

--- fp/src/components/Calculator.vue
+++ fp/src/components/Calculator.vue
@@ -1,40 +1,112 @@
 <template>
-   <div>
-       <div class="display">
-        <input v-model.number="operand1" type="number"/>
-        <input v-model.number="operand2" type="number"/>
-        = {{ result }}
-       </div>
-        <!-- 2 + 2 = {{ 2 + 2 }}
+  <div>
+    <div class="display">
+      <input v-model.number="operand1" type="number" />
+      <input v-model.number="operand2" type="number" />
+      = {{ result }}
+      = {{ fibResult }}
+    </div>
+    {{ fio }}
+    <div class="error" v-if="error">Ошбика {{ error }}</div>
+    <div class="error" v-show="error">Ошбика {{ error }}</div>
+    <div v-for="(item, id, idx) in logs" :key="`list_${id}`">
+      {{ id }}  - {{ item }} - {{ idx }}
+    </div>
+    <div class="strange-messages">
+      <template v-if="result < 0">Результат меньше 0</template>
+      <template v-else-if="result < 100">Результат в первой 100</template>
+      <template v-else>Остальной диапазон</template>
+    </div>
+    <!-- <input v-model.lazy="operand2" type="text" /> -->
+    <!-- 2 + 2 = {{ 2 + 2 }}
         {{ message }}
         {{ message.length }} -->
-       <div class="keyboard">
-        <button @click="result = operand1 + operand2">+</button>
-        <button @click="result = operand1 - operand2">-</button>
-        <button @click="divide">/</button>
-        <button @click="multiply(operand1, operand2)">*</button>
-       </div>
-   </div>
+    <div class="keyboard">
+      <button
+        v-for="operand in operands"
+        :key="operand"
+        @click="calculate(operand)"
+        :title="`Операция ${operand}`"
+        :disabled="operand === '/' && operand2 === 0"
+      >
+        {{ operand }}
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Calculator",
-    data() {
-        return {
-            // message: "asdasda asdasasdasd asdasd", 
-            operand1:0,
-            operand2:0,
-            result:0
-        }
+  name: "Calculator",
+  data() {
+    return {
+      // message: "asdasda asdasasdasd asdasd",
+      myCollection: [1, 2, 3, 4, 5, 6, 7],
+      operands: ["+", "-", "/", "*"],
+      operand1: 0,
+      operand2: 0,
+      error: "",
+      result: 0,
+      fibResult: 0,
+      lastName: "LastName",
+      userName: "User",
+      logs: {},
+    };
+  },
+  methods: {
+    calculate(operation = "+") {
+      this.error = "";
+      switch (operation) {
+        case "+":
+          this.add();
+          break;
+        case "-":
+          this.substract();
+          break;
+        case "*":
+          this.multiply();
+          break;
+        case "/":
+          this.divide();
+          break;
+      }
+
+      const key = Date.now();
+      const value = `${this.operand1} ${operation} ${this.operand2} = ${this.result}`;
+      this.$set(this.logs, key, value);
     },
-    methods: {
-        divide(){
-            this.result = this.operand1 / this.operand2
-        },
-        multiply(operand1,operand2){
-            this.result = operand1 * operand2
-        }
+    add() {
+      this.result = this.operand1 + this.operand2;
+      this.fibResult = this.fib1 + this.fib2
     },
-}
+    substract() {
+      this.result = this.operand1 - this.operand2;
+    },
+    divide() {
+      const { operand1, operand2 } = this;
+      if (operand2 === 0) {
+        this.error = "На 0 делить нельзя";
+        return;
+      }
+      this.result = operand1 / operand2;
+    },
+    multiply() {
+      this.result = this.operand1 * this.operand2;
+    },
+    fib(n){
+        return n <= 1 ? n : this.fib(n-1) + this.fib(n-2)
+    }
+  },
+  computed: {
+      fib1(){
+          return this.fib(this.operand1)
+      },
+      fib2(){
+          return this.fib(this.operand2)
+      },
+      fio(){
+          return this.userName && this.lastName ? `${this.lastName} ${this.userName}` : ''
+      }
+  }
+};
 </script>
