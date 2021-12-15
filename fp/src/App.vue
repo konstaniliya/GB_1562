db5ebe16380d46f8dfe78f5ffc8b68541569de91
Николай Федоров <nvfedorov@cloudmts.ru>
2021-12-15 20:08:35 +0300

    ls03

--- fp/src/App.vue
+++ fp/src/App.vue
@@ -1,25 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <calculator/>
+    <div class="wrapper">
+      <header>
+        <div class="title">My personal costs</div>
+      </header>
+      <main>
+        <add-payment-form @addNewPayment="addToPaymentList"/>
+        <payments-display :items="paymentsList" />
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-import Calculator from './components/Calculator.vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+import AddPaymentForm from "./components/AddPaymentForm.vue";
+import PaymentsDisplay from "./components/PaymentsDisplay.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    Calculator
-  }
-}
+  components: { PaymentsDisplay, AddPaymentForm },
+  name: "App",
+  data() {
+    return {
+      show: true,
+      paymentsList: [],
+    };
+  },
+  methods: {
+    fetchData() {
+      return [
+        {
+          date: "28.03.2020",
+          category: "Food",
+          value: 169,
+        },
+        {
+          date: "24.03.2020",
+          category: "Transport",
+          value: 360,
+        },
+        {
+          date: "24.03.2020",
+          category: "Food",
+          value: 532,
+        },
+        {
+          date: "24.03.2020",
+          category: "Internet",
+          value: 532,
+        },
+        {
+          date: "24.03.2020",
+          category: "Sport",
+          value: 532,
+        },
+        {
+          date: "24.03.2020",
+          category: "Food",
+          value: 532,
+        },
+      ];
+    },
+    addToPaymentList(props) {
+      console.log('run')
+      // this.paymentsList.push(data)
+      this.paymentsList = [...this.paymentsList, props];
+    },
+  },
+  created() {
+    this.paymentsList = this.fetchData();
+  },
+};
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -28,7 +80,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-h1{
+h1 {
   font-size: 20px;
 }
 </style>
---
+++ fp/src/components/MyButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <button @click="onClick">Clicked {{ counter }} times!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    onClick() {
+      this.counter++;
+    },
+    mouseOver(){
+        console.log('Hovered on button')
+    }
+  },
+  beforeCreate() {
+    console.log("beforeCreate");
+  },
+  created() {
+    console.log("created");
+  },
+  beforeMount() {
+    console.log("beforeMount");
+  },
+  mounted() {
+    console.log("mounted");
+    const btn = this.$el.querySelector('button')
+    if(btn) {
+        btn.addEventListener('mouseover', this.mouseOver)
+    }
+  },
+  beforeUpdate() {
+    console.log("beforeUpdate");
+  },
+  updated() {
+    console.log("updated");
+  },
+  beforeDestroy() {
+    const btn = this.$el.querySelector('button')
+    if(btn) {
+        btn.removeEventListener('mouseover', this.mouseOver)
+    }
+  },
+  destroyed() {
+    console.log("destroyed");
+  },
+};
+</script>
